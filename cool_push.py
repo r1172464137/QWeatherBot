f50fad56dcc0020c76cbb5260fe296b671c3ff11
magicFeirl <2100709458@qq.com>
2021-01-13 13:26:13 +0800

    post -> get，前者会自动编码 URL 导致文本显示错误（我暂且蒙古）

--- cool_push.py
+++ cool_push.py
@@ -3,17 +3,17 @@ import requests
 def send_msg_to_qq_group(KEY, content):
     group_api = 'https://push.xuthus.cc/group/{}'.format(KEY)
 
-    data = {
+    params = {
         'c': content
     }
 
-    return requests.post(group_api, data=data).json()
+    return requests.get(group_api, params=params).json()
 
 def send_msg_to_qq_user(KEY, content):
     group_api = 'https://push.xuthus.cc/send/{}'.format(KEY)
 
-    data = {
+    params = {
         'c': content
     }
 
-    return requests.post(group_api, data=data).json()
+    return requests.get(group_api, params=params).json()
