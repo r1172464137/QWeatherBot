6b332b183ae3ed13eb799e9a3bb3e3e9dac51523
Coupile <1172464137@qq.com>
2021-02-14 10:00:58 +0800

    Update cool_push.py

--- cool_push.py
+++ cool_push.py
@@ -2,7 +2,7 @@ import requests
 
 def send_msg_to_qq_group(content):
     group_api = 'https://push.godever.xyz/'
-
+    title = "天气情况"
     url= 'https://www.qweather.com/'
     params={
         'title':title,
